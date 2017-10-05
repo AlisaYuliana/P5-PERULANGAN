28cd6274171b82ea1de418d589e0a40e71b9fea8
AlisaYuliana <32087634+AlisaYuliana@users.noreply.github.com>
2017-10-05 22:08:36 +0700

    Add files via upload

---
+++ DemoFor.java
@@ -0,0 +1,21 @@
+public class DemoFor {
+
+    public static void main(String[] args) {
+        int i;
+        for (i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
+
+
+/**
+1. Deklarasi class dg nama DemoFor
+3. deklarasi method main untuk menjalankan class demoFor
+4. Deklarasi variabel i dengan type data integer
+5. perintah perulangan for dengan inisialisasi i=0, kondisi i<10, dan peubah i++
+6. mencetak variabel i
+7. penutup statement pada perulangan for 
+8. penutup method main
+9. penutup class DemoFor
+*/
